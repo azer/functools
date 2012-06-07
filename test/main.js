267c81de28db4716c8274913f189d09f2d793f97
E.Azer Koçulu <azer@kodfabrik.com>
2012-06-07 10:23:27 -0700

    highkick

--- test/main.js
+++ test/main.js
@@ -1,85 +1,116 @@
-if(typeof require!='undefined'){
-  var functools = require("../lib/functools"),
-      assert = require("assert");
+var node = typeof process != 'undefined' && process.versions && process.versions.node != undefined;
+
+if(node){
+  assert  = require('assert');
+  functools = require("../lib/functools");
+} else {
+  module = {};
+  assert = {
+    'equal': function(a, b){
+      if( a !== b ){
+        throw new Error('Assertion Error: '+ a +' != ' + b);
+      }
+    }
+  };
 }
 
-function test_compose(){
+
+function cube(x){ return x*x*x; };
+function sum(x){ return x+x; };
+function div(x){ return x/2; };
+function mul(x){return x*3; };
+
+function cubeAsync(x,callback){ callback(undefined,x*x*x); };
+function sumAsync(x,callback){ callback(undefined,x+x); };
+function divAsync(x,callback){ callback(undefined,x/2); };
+function mulAsync(x,callback){ callback(undefined,x*3); };
+
+function test_compose(callback){
 
   var unaltered = Math.floor(Math.random()*100);
 
   var increment = function(serial){
     return function(val){
       return val+serial;
-    }
-  }
+    };
+  };
 
   assert.equal(functools.compose(increment(1), increment(2), increment(3) )(unaltered),unaltered+6);
 
-  function cube(x){ return x*x*x };
-  function sum(x){ return x+x; };
-  function div(x){ return x/2; }
-  function mul(x){return x*3; }
   assert.equal(functools.compose(cube, sum, div, mul)(2), 24);
+
+  callback();
 };
 
-function test_compose_async(){
+function test_compose_async(callback){
   var unaltered = Math.floor(Math.random()*100);
 
   function increment(serial){
     return function(val,callback){
       callback(undefined, val+serial);
-    }
+    };
   }
 
   functools.compose.async( increment(1), increment(2), increment(3) )(unaltered,function(error,result){
     if(error){
-      throw error;
+      callback(error);
+      return;
     }
 
     assert.equal(result, unaltered + 6);
-  });
 
-  function cube(x,callback){ callback(undefined,x*x*x) };
-  function sum(x,callback){ callback(undefined,x+x); };
-  function div(x,callback){ callback(undefined,x/2); }
-  function mul(x,callback){ callback(undefined,x*3); }
-
-  functools.compose.async(cube, sum, div, mul)(2,function(error, result){
-    assert.equal(result, 24);
+    functools.compose.async(cubeAsync, sumAsync, divAsync, mulAsync)(2,function(error, result){
+      if(error){
+        callback(error);
+        return;
+      }
+      assert.equal(result, 24);
+      callback();
+    });
   });
 }
 
-function test_compose_async_error(){
-  functools.compose.async(function(_,callback){
-    callback(new Error('foobar'),9);
-  },[3,1,4],function(error, result){
+function test_compose_async_error(callback){
+  function err(_,callback){ 
+    callback(new Error('foobar'),9); 
+  };
+
+  functools.compose.async(err)([3,1,4], function(error, result){
     assert.equal(error.message, 'foobar');
+    callback();
   });
 }
 
-function test_curry(){
+function test_curry(callback){
+
   function sum(a,b,c){
     return a+b+c;
- }
+  }
 
   assert.equal( functools.curry(sum)()(3)()(1)(4),8);
   assert.equal( functools.curry(sum,3,1,4),8);
   assert.equal( functools.curry(sum,3)(1,4),8);
 
+  callback();
 }
 
-function test_each(){
+function test_each(callback){
   var range = [3,1,4,1,5,9],
       serial = 0;
+
   assert.equal(functools.each(function(el,ind,seq){
+
     assert.equal(seq,range);
     assert.equal(ind,serial++);
     assert.ok(ind<seq.length);
+
   },range),range);
+
+  callback();
 
 }
 
-function test_filter(){
+function test_filter(callback){
   var range = [3,1,4,1,5,9],
       rangeClone = Array.prototype.slice.call(range),
       serial = 0;
@@ -87,9 +118,11 @@ function test_filter(){
   var evensInRange = functools.filter(function(el,ind,seq){
 
     assert.equal(range.length, rangeClone.length);
-    for(var i = -1, len=rangeClone.length; ++i < len; ){
+
+    var i = rangeClone.length;
+    while( i -- ){
       assert.equal(rangeClone[i],range[i]);
-    };
+    }
 
     assert.equal(serial++,ind);
     assert.equal(range[ind], el);
@@ -100,9 +133,11 @@ function test_filter(){
 
   assert.equal(evensInRange.length, 1);
   assert.equal(evensInRange[0], 4);
+
+  callback();
 };
 
-function test_filter_async(){
+function test_filter_async(callback){
   var range = [3,1,4,1,5,9],
       rangeClone = Array.prototype.slice.call(range),
       serial = 0;
@@ -112,22 +147,21 @@ function test_filter_async(){
   },range, function(evensInRange){
     assert.equal(evensInRange.length, 1);
     assert.equal(evensInRange[0], 4);
+
+    callback();
   });
 }
 
-function test_juxt(){
-  var unaltered = Math.floor(Math.random()*100);
+function test_juxt(callback){
+
   function inc(serial){ return function(val){ return val+serial; } }
+
+  var unaltered = Math.floor(Math.random()*100);
 
   var result = functools.juxt(inc(1), inc(2), inc(3) )(unaltered);
   assert.equal(result[0], unaltered+1);
   assert.equal(result[1], unaltered+2);
   assert.equal(result[2], unaltered+3);
-
-  function cube(x){ return x*x*x };
-  function sum(x){ return x+x; };
-  function div(x){ return x/2; }
-  function mul(x){return x*3; }
 
   result = functools.juxt(cube, sum, div, mul)(2);
   assert.equal(result[0], 8);
@@ -141,47 +175,69 @@ function test_juxt(){
   assert.equal(result.div, 1);
   assert.equal(result.mul, 6);
 
+  callback();
+
 }
 
-function test_juxt_async(){
+function test_juxt_async(callback){
   var unaltered = Math.floor(Math.random()*100);
 
-  function inc(serial){ return function(val, callback){ callback(undefined, val+serial); } }
+  function inc(serial){
+    return function(val, callback){
+      callback(undefined, val+serial);
+    };
+  };
 
   functools.juxt.async( inc(1), inc(2), inc(3) )(unaltered,function(error,result){
+
     if(error){
-      throw error;
+      callback(error);
+      return;
     }
 
     assert.equal(result[0], unaltered+1);
     assert.equal(result[1], unaltered+2);
     assert.equal(result[2], unaltered+3);
+
+    console.log(2);
+
+    functools.juxt.async(cubeAsync, sumAsync, divAsync, mulAsync)(2,function(error, result){
+
+      if(error){
+        callback(error);
+        return;
+      }
+
+      assert.equal(result[0], 8);
+      assert.equal(result[1], 4);
+      assert.equal(result[2], 1);
+      assert.equal(result[3], 6);
+
+      functools.juxt.async({ 'cube':cubeAsync, 'sum':sumAsync, 'div':divAsync, 'mul':mulAsync })(2,function(error, result){
+
+        if(error){
+          callback(error);
+          return;
+        }
+
+        assert.equal(result.cube, 8);
+        assert.equal(result.sum, 4);
+        assert.equal(result.div, 1);
+        assert.equal(result.mul, 6);
+
+        callback();
+
+      });
+
+    });
+
   });
 
-  function cube(x,callback){ callback(undefined,x*x*x) };
-  function sum(x,callback){ callback(undefined,x+x); };
-  function div(x,callback){ callback(undefined,x/2); }
-  function mul(x,callback){ callback(undefined,x*3); }
 
-  functools.juxt.async(cube, sum, div, mul)(2,function(error, result){
-    if(error) throw error;
-    assert.equal(result[0], 8);
-    assert.equal(result[1], 4);
-    assert.equal(result[2], 1);
-    assert.equal(result[3], 6);
-  });
-
-  functools.juxt.async({ 'cube':cube, 'sum':sum, 'div':div, 'mul':mul })(2,function(error, result){
-    if(error) throw error;
-    assert.equal(result.cube, 8);
-    assert.equal(result.sum, 4);
-    assert.equal(result.div, 1);
-    assert.equal(result.mul, 6);
-  });
 
 }
 
-function test_map(){
+function test_map(callback){
 
   var range = [3,1,4],
       serial = 0;
@@ -222,9 +278,11 @@ function test_map(){
   assert.equal(seq.b, 1);
   assert.equal(seq.c, 16);
 
+  callback();
+
 };
 
-function test_map_async(){
+function test_map_async(callback){
 
   var range = [3,1,4];
 
@@ -243,44 +301,47 @@ function test_map_async(){
     assert.equal(seq[0], 9);
     assert.equal(seq[1], 1);
     assert.equal(seq[2], 16);
-  });
 
-  range = { 'a': 3, 'b': 1, 'c': 4 };
+    range = { 'a': 3, 'b': 1, 'c': 4 };
 
-  functools.map.async(function(el,callback){ callback(undefined, el*el); }, range, function(error, seq){
+    functools.map.async(function(el,callback){ callback(undefined, el*el); }, range, function(error, seq){
 
-    if(error){
-      throw error;
-    }
+      if(error){
+        throw error;
+      }
 
-    assert.equal(range.a, 3);
-    assert.equal(range.b, 1);
-    assert.equal(range.c, 4);
+      assert.equal(range.a, 3);
+      assert.equal(range.b, 1);
+      assert.equal(range.c, 4);
 
-    assert.equal(seq.a, 9);
-    assert.equal(seq.b, 1);
-    assert.equal(seq.c, 16);
+      assert.equal(seq.a, 9);
+      assert.equal(seq.b, 1);
+      assert.equal(seq.c, 16);
+
+      callback();
+
+    });
+
   });
 
 };
 
-function test_map_async_error(){
-  functools.map.async(function(_,callback){
-    callback(new Error('foobar'),9);
-  },[3,1,4],function(error, list){
+function test_map_async_error(callback){
+  functools.map.async(function(_,callback){ callback(new Error('foobar'),9); },[3,1,4],function(error, list){
     assert.equal(list[0], 9);
     assert.equal(list[1], 1);
     assert.equal(list[2], 4);
 
     assert.equal(error.message, 'foobar');
+    callback();
   });
 }
 
 
-function test_partial(){
+function test_partial(callback){
   var sum = function(){
     return functools.reduce(function(x,y){ return x+y },arguments);
-  }
+  };
 
   assert.equal(functools.partial(sum,[3,1,4,0])(),8);
   assert.equal(functools.partial(sum)(3,1,4,0),8);
@@ -292,39 +353,44 @@ function test_partial(){
     assert.equal(this, foo);
   },[],foo)();
 
+  callback();
+
 };
 
-function test_reduce(){
+function test_reduce(callback){
+
   var range = [3,1,4,1,5,9];
+
   assert.equal(functools.reduce(function(x,y,seq){
     return x*y;
   },range),540);
 
+  callback();
+
 };
 
-function test_reduce_async(){
+function test_reduce_async(callback){
   var range = [3,1,4,1,5,9];
-  functools.reduce.async(function(x,y,callback){
-    callback(undefined, x*y);
-  },range,function(error, result){
+  functools.reduce.async(function(x,y,callback){ callback(undefined, x*y); }, range, function(error, result){
+
     if(error){
       throw error;
     }
+
     assert.equal(result, 540);
+    callback();
   });
-
 };
 
-function test_reduce_async_error(){
+function test_reduce_async_error(callback){
   var range = [3,1,4,1,5,9];
-  functools.reduce.async(function(_,_,callback){
-    callback(new Error('foobar'));
-  },range,function(error, result){
+  functools.reduce.async(function(_,_,callback){ callback(new Error('foobar')); }, range, function(error, result){
     assert.equal(error.message, 'foobar');
+    callback();
   });
 };
 
-var tests = {
+module.exports = {
   'test_compose':test_compose,
   'test_compose_async':test_compose_async,
   'test_compose_async_error':test_compose_async_error,
@@ -343,5 +409,3 @@ var tests = {
   'test_reduce_async_error':test_reduce_async_error,
   'test_partial':test_partial
 };
-
-typeof module != 'undefined' && ( module.exports = tests );
